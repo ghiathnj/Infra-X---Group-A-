77d7000187f6b2e5728cfbd2d17a95621986c02e
ghiathnj88 <ghiathnj88@gmail.com>
2026-04-20 20:42:52 +0200

    Revert "feat(form-submission): add service with full backend"
    This reverts commit 0010198fb34c86a6684b7c9ad3a6fe49a62f9b86.

--- db-init/01-create-databases.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS auth_db
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
-CREATE DATABASE IF NOT EXISTS form_db
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
